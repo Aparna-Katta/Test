0f5e663bacef9e888b732d4b75a0b3a9f95d3899
Aparna-Katta <aparna.katta@excelicare.com>
2017-05-09 15:10:47 +0530

    First Commit

---
+++ CodeExecution.bat
@@ -0,0 +1,9 @@
+cd %~dp0
+SET CLASSPATH=..\bin;
+SET PATH=%PATH%;
+cls
+
+java -cp ".\bin;" Demo.Practice1
+
+cd..
+pause
---
+++ src/Demo/Practice1.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Practice1 {
+
+	public static void main(String[] args) throws Exception{
+		System.out.println("Sample to check Git Push");
+		
+
+	}
+
+}
